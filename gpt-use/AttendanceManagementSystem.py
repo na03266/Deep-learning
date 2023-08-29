e8d2f644e7eb374b9b4dcd8e4ed272aed3d8758f
NaHwangje <na03266@naver.com>
2023-08-29 09:54:16 +0900

    근태관리 시스템 기본 정의서 및 1번 기능 구현

--- gpt-use/AttendanceManagementSystem.py
+++ gpt-use/AttendanceManagementSystem.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from datetime import datetime
+from tkcalendar import DateEntry
 
 class AttendanceManagementSystem:
     def __init__(self, root):
@@ -20,6 +21,15 @@ class AttendanceManagementSystem:
 
         self.show_attendance_button = tk.Button(root, text="Show Attendance", command=self.show_attendance)
         self.show_attendance_button.pack()
+
+        self.label_date = tk.Label(root, text="Select Date:")
+        self.label_date.pack()
+
+        self.selected_date = DateEntry(root, date_pattern="yyyy-mm-dd")
+        self.selected_date.pack()
+
+        self.show_attendance_by_date_button = tk.Button(root, text="Show Attendance by Date", command=self.show_attendance_by_date)
+        self.show_attendance_by_date_button.pack()
 
     def mark_attendance(self):
         employee_id = self.employee_id_entry.get()
@@ -42,6 +52,22 @@ class AttendanceManagementSystem:
                 messagebox.showinfo("Attendance for Employee ID: " + employee_id, attendance_list)
             else:
                 messagebox.showinfo("Attendance for Employee ID: " + employee_id, "No attendance recorded")
+        else:
+            messagebox.showerror("Error", "Please enter Employee ID")
+
+    def show_attendance_by_date(self):
+        selected_date_str = self.selected_date.get_date().strftime("%Y-%m-%d")
+        employee_id = self.employee_id_entry.get()
+        if employee_id:
+            if employee_id in self.attendance_data:
+                attendance_list = [time for time in self.attendance_data[employee_id] if selected_date_str in time]
+                if attendance_list:
+                    attendance_info = "\n".join(attendance_list)
+                    messagebox.showinfo("Attendance for Employee ID: " + employee_id + " on " + selected_date_str, attendance_info)
+                else:
+                    messagebox.showinfo("Attendance for Employee ID: " + employee_id + " on " + selected_date_str, "No attendance recorded on selected date")
+            else:
+                messagebox.showinfo("Attendance for Employee ID: " + employee_id + " on " + selected_date_str, "No attendance recorded")
         else:
             messagebox.showerror("Error", "Please enter Employee ID")
 
