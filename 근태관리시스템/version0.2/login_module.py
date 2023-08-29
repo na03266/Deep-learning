7dc6fcee1882b1df40730dd16af0221b4703e698
NaHwangje <na03266@naver.com>
2023-08-29 12:07:40 +0900

    근태관리시스템 버전 분할

---
+++ 근태관리시스템/version0.2/login_module.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class UserAuthenticationApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("사용자 인증 및 로그인")
+        
+        self.username_label = tk.Label(root, text="사용자명:")
+        self.username_label.pack()
+
+        self.username_entry = tk.Entry(root)
+        self.username_entry.pack()
+
+        self.password_label = tk.Label(root, text="비밀번호:")
+        self.password_label.pack()
+
+        self.password_entry = tk.Entry(root, show="*")
+        self.password_entry.pack()
+
+        self.login_button = tk.Button(root, text="로그인", command=self.login)
+        self.login_button.pack()
+
+    def login(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        if username == "admin" and password == "password":
+            messagebox.showinfo("성공", "로그인 성공!")
+        else:
+            messagebox.showerror("오류", "사용자명 또는 비밀번호가 잘못되었습니다.")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = UserAuthenticationApp(root)
+    root.mainloop()
