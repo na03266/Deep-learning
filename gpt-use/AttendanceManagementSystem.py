d7e0e1420a41428080b2561b474f4da66410aee2
NaHwangje <na03266@naver.com>
2023-08-29 09:59:42 +0900

    로그인 기능 추가

--- gpt-use/AttendanceManagementSystem.py
+++ gpt-use/AttendanceManagementSystem.py
@@ -9,18 +9,23 @@ class AttendanceManagementSystem:
         self.root.title("Attendance Management System")
         
         self.attendance_data = {}  # 근태 데이터를 저장할 딕셔너리
+        self.user_data = {"admin": {"password": "admin123", "role": "admin"},
+                          "employee": {"password": "employee123", "role": "employee"}}
         
-        self.label = tk.Label(root, text="Enter Employee ID:")
-        self.label.pack()
+        self.label_id = tk.Label(root, text="Enter User ID:")
+        self.label_id.pack()
 
-        self.employee_id_entry = tk.Entry(root)
-        self.employee_id_entry.pack()
+        self.user_id_entry = tk.Entry(root)
+        self.user_id_entry.pack()
 
-        self.mark_attendance_button = tk.Button(root, text="Mark Attendance", command=self.mark_attendance)
-        self.mark_attendance_button.pack()
+        self.label_password = tk.Label(root, text="Enter Password:")
+        self.label_password.pack()
 
-        self.show_attendance_button = tk.Button(root, text="Show Attendance", command=self.show_attendance)
-        self.show_attendance_button.pack()
+        self.password_entry = tk.Entry(root, show="*")
+        self.password_entry.pack()
+
+        self.login_button = tk.Button(root, text="Login", command=self.login)
+        self.login_button.pack()
 
         self.label_date = tk.Label(root, text="Select Date:")
         self.label_date.pack()
@@ -28,48 +33,64 @@ class AttendanceManagementSystem:
         self.selected_date = DateEntry(root, date_pattern="yyyy-mm-dd")
         self.selected_date.pack()
 
-        self.show_attendance_by_date_button = tk.Button(root, text="Show Attendance by Date", command=self.show_attendance_by_date)
-        self.show_attendance_by_date_button.pack()
+        self.show_attendance_button = tk.Button(root, text="Show My Attendance", command=self.show_attendance)
+        self.show_attendance_button.pack()
 
-    def mark_attendance(self):
-        employee_id = self.employee_id_entry.get()
-        if employee_id:
-            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            if employee_id in self.attendance_data:
-                self.attendance_data[employee_id].append(current_time)
+        self.role = ""
+        
+    def login(self):
+        user_id = self.user_id_entry.get()
+        password = self.password_entry.get()
+        
+        if user_id and password:
+            if user_id in self.user_data:
+                if self.user_data[user_id]["password"] == password:
+                    self.role = self.user_data[user_id]["role"]
+                    self.show_main_menu()
+                else:
+                    messagebox.showerror("Error", "Invalid password")
             else:
-                self.attendance_data[employee_id] = [current_time]
-            
-            messagebox.showinfo("Success", "Attendance marked for Employee ID: " + employee_id)
+                messagebox.showerror("Error", "Invalid user ID")
         else:
-            messagebox.showerror("Error", "Please enter Employee ID")
+            messagebox.showerror("Error", "Please enter user ID and password")
+
+    def show_main_menu(self):
+        self.label_id.pack_forget()
+        self.user_id_entry.pack_forget()
+        self.label_password.pack_forget()
+        self.password_entry.pack_forget()
+        self.login_button.pack_forget()
+
+        if self.role == "admin":
+            self.show_admin_menu()
+        elif self.role == "employee":
+            self.show_employee_menu()
+
+    def show_admin_menu(self):
+        self.label_date.pack()
+        self.selected_date.pack()
+        self.show_attendance_button.pack()
+
+    def show_employee_menu(self):
+        self.label_date.pack()
+        self.selected_date.pack()
+        self.show_attendance_button.pack()
 
     def show_attendance(self):
-        employee_id = self.employee_id_entry.get()
-        if employee_id:
-            if employee_id in self.attendance_data:
-                attendance_list = "\n".join(self.attendance_data[employee_id])
-                messagebox.showinfo("Attendance for Employee ID: " + employee_id, attendance_list)
-            else:
-                messagebox.showinfo("Attendance for Employee ID: " + employee_id, "No attendance recorded")
-        else:
-            messagebox.showerror("Error", "Please enter Employee ID")
-
-    def show_attendance_by_date(self):
         selected_date_str = self.selected_date.get_date().strftime("%Y-%m-%d")
-        employee_id = self.employee_id_entry.get()
-        if employee_id:
-            if employee_id in self.attendance_data:
-                attendance_list = [time for time in self.attendance_data[employee_id] if selected_date_str in time]
+        user_id = self.user_id_entry.get()
+        if user_id:
+            if user_id in self.attendance_data:
+                attendance_list = [time for time in self.attendance_data[user_id] if selected_date_str in time]
                 if attendance_list:
                     attendance_info = "\n".join(attendance_list)
-                    messagebox.showinfo("Attendance for Employee ID: " + employee_id + " on " + selected_date_str, attendance_info)
+                    messagebox.showinfo("Attendance for User ID: " + user_id + " on " + selected_date_str, attendance_info)
                 else:
-                    messagebox.showinfo("Attendance for Employee ID: " + employee_id + " on " + selected_date_str, "No attendance recorded on selected date")
+                    messagebox.showinfo("Attendance for User ID: " + user_id + " on " + selected_date_str, "No attendance recorded on selected date")
             else:
-                messagebox.showinfo("Attendance for Employee ID: " + employee_id + " on " + selected_date_str, "No attendance recorded")
+                messagebox.showinfo("Attendance for User ID: " + user_id + " on " + selected_date_str, "No attendance recorded")
         else:
-            messagebox.showerror("Error", "Please enter Employee ID")
+            messagebox.showerror("Error", "Please enter User ID")
 
 if __name__ == "__main__":
     root = tk.Tk()
