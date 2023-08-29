6ad0c9f96d2eaf30961bcc18600b40b7f720032c
NaHwangje <na03266@naver.com>
2023-08-29 12:16:53 +0900

    로그인, 회원가입 모듈 구현

---
+++ 근태관리시스템/version0.2/registration/user_registration.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class UserRegistrationApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("회원 등록")
+        
+        self.username_label = tk.Label(root, text="사용자명:")
+        self.username_label.pack()
+
+        self.username_entry = tk.Entry(root)
+        self.username_entry.pack()
+
+        self.password_label = tk.Label(root, text="비밀번호:")
+        self.password_label.pack()
+
+        self.password_entry = tk.Entry(root, show="*")
+        self.password_entry.pack()
+
+        self.register_button = tk.Button(root, text="회원 등록", command=self.register)
+        self.register_button.pack()
+
+    def register(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        # 여기서 데이터베이스에 사용자 정보를 저장하는 로직을 추가할 수 있습니다.
+        messagebox.showinfo("성공", "회원 등록이 완료되었습니다.")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = UserRegistrationApp(root)
+    root.mainloop()
