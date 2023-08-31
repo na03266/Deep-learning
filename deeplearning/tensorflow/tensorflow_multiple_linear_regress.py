f520512653a005b7f4d6a36833bb30bd12ed6375
NaHwangje <na03266@naver.com>
2023-08-31 11:30:15 +0900

    파일 목차 분류

---
+++ deeplearning/tensorflow/tensorflow_multiple_linear_regress.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 텐서플로의 케ㅔ라스 API에서 필요한 함수 호출
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+x = np.array([[2,0],[4,4],[6,2],[8,3]])
+y = np.array([81,93,91,97])
+
+model = Sequential()
+
+# 출력 값, 입력변수(학습시간, 과외시간), 분석 방법에 맞게끔 모델 설정
+model.add(Dense(1, input_dim=2, activation='linear'))
+
+# 오차 수정을 위해 경사 하강법(sgd), 오차의 정도 판단을 위해 평균제곱오차 사용
+model.compile(optimizer='sgd', loss='mse')
+
+#오차를 최소화하는 과정 2000번 반복
+model.fit(x,y,epochs=2000)
+
+
+#임의의 시간을 집어넣어 점수를 예측하는 모델 테스트
+hour = 7
+private_class = 4
+prediction = model.predict([[hour, private_class]])
+print("%.f시간을 공부하고, %.f시간의 과외를 받을 경우, 예상 점수는 %.02f점입니다." 
+      % (hour,private_class, prediction))
